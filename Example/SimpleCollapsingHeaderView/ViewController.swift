4abca39279d0f745a860da8a559a5263bdbaa940
Nixsm <nichsmm@gmail.com>
2018-01-13 18:59:26 -0200

    Add Example code

--- Example/SimpleCollapsingHeaderView/ViewController.swift
+++ Example/SimpleCollapsingHeaderView/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SimpleCollapsingHeaderView
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var headerView: SimpleCollapsingHeaderView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        headerView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,3 +30,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        headerView?.collapseHeaderView(using: scrollView)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
+
+extension ViewController: SimpleCollapsingHeaderViewDelegate {
+    func onHeaderDidAnimate(with percentage: CGFloat) {
+        titleLabel.alpha = percentage
+    }
+}
